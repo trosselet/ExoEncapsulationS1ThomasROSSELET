f4dda5cba17ffbe514c09ab624a3ec4ee316485a
Thomas <147041393+trosselet@users.noreply.github.com>
2024-10-11 17:44:49 +0200

    Delete ExoEncapsulation directory

--- ExoEncapsulation/ExoEncapsulation/StaticObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "StaticObject.h"
-#include <iostream>
-
-StaticObject::StaticObject(float x, float y) : Entity(x, y)
-{
-	std::cout << "Static Object just created at x = " << x << " y = " << y << std::endl;
-}
